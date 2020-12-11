934566f2a70722b7e036dc2c2fa55dcd741875c9
Ashuka Xue <axue@vmware.com>
2020-12-10 17:42:47 -0800

    TestSimple is working!

--- server/pxf-hive/src/test/java/org/greenplum/pxf/plugins/hive/HiveResolverTest.java
+++ server/pxf-hive/src/test/java/org/greenplum/pxf/plugins/hive/HiveResolverTest.java
@@ -2,10 +2,14 @@ package org.greenplum.pxf.plugins.hive;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.serde.serdeConstants;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.greenplum.pxf.api.io.DataType;
 import org.greenplum.pxf.api.model.RequestContext;
 import org.greenplum.pxf.api.OneField;
 import org.greenplum.pxf.api.OneRow;
+import org.greenplum.pxf.api.utilities.ColumnDescriptor;
 import org.greenplum.pxf.plugins.hive.utilities.HiveUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,29 +17,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class HiveResolverTest {
-    private static final String COLUMN_NAMES = "id,name,dec1";
-    private static final String COLUMN_TYPES = "int:string:decimal(38,18)";
 
     @Mock
     HiveUtilities mockHiveUtilities;
 
-    @Mock
-    RequestContext mockRequestContext;
-
     private static final String SERDE_CLASS_NAME = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe";
+    private static final String COLUMN_NAMES = "name,amt";
+    private static final String COLUMN_TYPES = "string:double";
 
     Configuration configuration;
     Properties properties;
-//    private RequestContext context;
+    List<ColumnDescriptor> columnDescriptors;
     private HiveResolver resolver;
     private OneRow row;
 
@@ -47,16 +49,22 @@ public class HiveResolverTest {
         properties.put(serdeConstants.LIST_COLUMN_TYPES, COLUMN_TYPES);
         configuration = new Configuration();
 
-        row = new OneRow("0.0", new Text("value1"));
+        columnDescriptors = new ArrayList<>();
+        columnDescriptors.add(new ColumnDescriptor("name", DataType.TEXT.getOID(), 1, "text", null));
+        columnDescriptors.add(new ColumnDescriptor("amt", DataType.FLOAT8.getOID(), 3, "float8", null));
+
+        ArrayWritable aw = new ArrayWritable(Text.class, new Writable[]{new Text("value1")});
+        row = new OneRow(aw);
     }
 
     @Test
     public void testSimple() throws Exception {
-//        when(mockRequestContext.getConfiguration()).thenReturn(configuration);
         RequestContext context = new RequestContext();
         context.setConfiguration(configuration);
         // metadata usually set in accessor
-        context.setMetadata(new HiveMetadata(properties, null /*List<HivePartition>*/, null /*List<Integer>*/));
+        List<Integer> hiveIndexes = Arrays.asList(0, 1);
+        context.setMetadata(new HiveMetadata(properties, null /*List<HivePartition>*/, hiveIndexes));
+        context.setTupleDescription(columnDescriptors);
         resolver = new HiveResolver(mockHiveUtilities);
         resolver.setRequestContext(context);
         resolver.afterPropertiesSet();
